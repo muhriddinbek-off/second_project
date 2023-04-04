a9fac97304ce3bf83e86cd93d14e08f77d644ce9
muhriddinbek-off <muhriddinusanov7@gmail.com>
2023-04-04 14:33:16 +0500

    second

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    Container(
-      color: Colors.black,
+  Container box1 = Container(
+    color: Colors.green,
+    margin: EdgeInsets.fromLTRB(30, 120, 30, 120),
+    child: Text(
+      'Hello World',
+      textDirection: TextDirection.ltr,
+      textAlign: TextAlign.center,
+      style: TextStyle(fontSize: 50, color: Colors.black,
+      fontWeight: FontWeight.w700),
     ),
+    padding: EdgeInsets.fromLTRB(10, 250, 10, 200),
   );
+  Container box = Container(
+    color: Colors.yellow,
+    child: box1,
+  );
+  runApp(box);
 }
