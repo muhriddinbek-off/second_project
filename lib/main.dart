82b42426d6611849934aaa800fa20e3dc9ed140a
muhriddinbek-off <muhriddinusanov7@gmail.com>
2023-04-05 11:40:50 +0500

    second project

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,61 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  Container box1 = Container(
-    color: Colors.green,
-    margin: EdgeInsets.fromLTRB(30, 120, 30, 120),
+void main(){
+  Container box2 = Container(
+    margin: EdgeInsets.fromLTRB(20, 100, 20, 100),
+    decoration: BoxDecoration(
+      gradient: LinearGradient(begin: Alignment.bottomRight,
+      end: Alignment.topLeft,
+      colors: [
+        Color.fromARGB(255, 129, 71, 67),
+        Color.fromARGB(255, 243, 223, 5),
+      ]),
+      borderRadius: BorderRadius.only(
+        bottomLeft: Radius.circular(160),
+        bottomRight: Radius.circular(40),
+        topLeft: Radius.circular(40),
+        topRight: Radius.circular(160),
+      ),
+    ),
     child: Text(
       'Hello World',
       textDirection: TextDirection.ltr,
-      textAlign: TextAlign.center,
-      style: TextStyle(fontSize: 50, color: Colors.black,
+      style: TextStyle(fontSize: 35,
+      color: Colors.black,
       fontWeight: FontWeight.w700),
     ),
-    padding: EdgeInsets.fromLTRB(10, 250, 10, 200),
+    alignment: Alignment(-0.2, 0.0),
+  );
+  Container box1 =Container(
+    margin: EdgeInsets.fromLTRB(30, 160, 30, 160),
+    decoration: BoxDecoration(
+      gradient: LinearGradient(begin: Alignment.bottomRight,
+      end: Alignment.topLeft,
+      colors: [
+        Color.fromARGB(255, 211, 187, 187),
+        Color.fromARGB(255, 4, 57, 65),
+      ]),
+      borderRadius: BorderRadius.circular(50),
+      color: Colors.green,
+      border: Border.all(
+        color: Colors.blue,
+        style: BorderStyle.solid,
+        width: 10,
+      ),
+    ),
+    child: box2,
   );
   Container box = Container(
-    color: Colors.yellow,
+    decoration: BoxDecoration(
+      gradient: LinearGradient(
+        begin: Alignment.bottomCenter,
+        end: Alignment.topCenter,
+        colors: [
+          Colors.blue,
+          Color.fromARGB(255, 93, 122, 0),
+        ],
+      ),
+    ),
     child: box1,
   );
   runApp(box);
